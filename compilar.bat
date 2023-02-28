4572b4f135344d972b2d00e24ae5f288a530a268
Yiecheverria <111477598+Yiecheverria@users.noreply.github.com>
2023-02-28 18:15:49 -0500

    Update compilar.bat

--- compilar.bat
+++ compilar.bat
@@ -1,2 +1,2 @@
-git clone https://github.com/Yiecheverria/Proyecto-Grupo-9.git && cd ./Proyecto-Grupo-9 && cd ./app-books && gradlew libertyPackage && docker build -t jaimesalvador/app-books:1.0.0 . && cd .. && cd ./app-authors && docker build -t jaimesalvador/app-authors:1.0.0 . && cd .. && docker push jaimesalvador/app-books:1.0.0 && docker push jaimesalvador/app-authors:1.0.0 && docker compose up
+git clone https://github.com/Yiecheverria/Proyecto-Grupo-9.git && cd ./Proyecto-Grupo-9 && cd ./app-books && gradlew libertyPackage && docker build -t yiecheverria/app-books:1.0.0 . && cd .. && cd ./app-authors && docker build -t yiecheverria/app-authors:1.0.0 . && cd .. && docker push yiecheverria/app-books:1.0.0 && docker push yiecheverria/app-authors:1.0.0 && docker compose up
 
